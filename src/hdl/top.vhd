3b0a361a3cc27c09bdfafdbc3b2dd5754799fd07
mohammad orouji <mohammadorouji75@gmail.com>
2023-03-20 17:50:03 +0330

    create build.tcl

--- src/hdl/top.vhd
+++ src/hdl/top.vhd
@@ -4,11 +4,11 @@ use IEEE.std_logic_unsigned.all;
 
 entity top is
     port (
-        RESET_TOP                 :   in      std_logic;
-        CLK_TOP                   :   in      std_logic;
-        ENABLE_TOP                :   in      std_logic;
-        START_SEN_TOP             :   in      std_logic;
-        DATA_RECEIVED_of_master   :   out     std_logic_vector(7 downto 0)
+        RESET_TOP       :   in      std_logic;
+        CLK_TOP         :   in      std_logic;
+        ENABLE_TOP      :   in      std_logic;
+        START_SEN_TOP   :   in      std_logic;
+        ERROR_TOP       :   out     std_logic_vector(3 downto 0)
     );
 end top;
 
@@ -21,9 +21,11 @@ architecture structrual of top is
     signal DATA_rec_of_slave    : std_logic_vector(7 downto 0) := (others => '0') ;
 
     --spi slave
-    signal data_send_of_master : std_logic_vector(7 downto 0) := "01010101" ;
-    signal ready_sender_slave  : std_logic;
-    signal valid_sender_slave  : std_logic := '1';
+    signal data_err             : std_logic_vector(7 downto 0);
+
+    signal data_send_of_master  : std_logic_vector(7 downto 0) := "01010101" ;
+    signal ready_sender_slave   : std_logic;
+    signal valid_sender_slave   : std_logic := '1';
 
     signal ready_err    : std_logic := '1';
     signal valid_err    : std_logic;
@@ -35,18 +37,20 @@ architecture structrual of top is
 
 begin
 
-        ins0_counter : entity  work.COUNTER
-            generic map( 
-                reg_width_data => 8
-            )    
-            port map(
-                ENABLE_I    => ENABLE_TOP,
-                RESET_I     => RESET_TOP,
-                CLK_I       => CLK_TOP,
-                READY_I     => ready_counter,
-                VALID_O     => valid_counter,
-                DATA_O      => DATA_counter
-            );        
+    ins0_counter : entity  work.COUNTER
+        generic map( 
+            reg_width_data => 8
+        )    
+        port map(
+            ENABLE_I    => ENABLE_TOP,
+            RESET_I     => RESET_TOP,
+            CLK_I       => CLK_TOP,
+            READY_I     => ready_counter,
+            VALID_O     => valid_counter,
+            DATA_O      => DATA_counter
+        );  
+        
+
     
     ins0_spi_master : entity work.spi_master 
         generic map(
@@ -77,32 +81,46 @@ begin
             -- VALID_O         : out std_logic
         );
 
-        ins0_spi_slave : entity work.spi_slave 
-            generic map(
-                data_TX_spi_slave_reg_width => 8,
-                data_RX_spi_slave_reg_width => 8,
-                CPOL                        => '1',
-                CPHA                        => '0'
-                -- MSB_first       : integer := 1 
-                -- LSB_first       : integer := 0
-            )
-            port map(
-                CLK_I           => CLK_TOP,
-                RESET_I         => RESET_TOP,
-                ------------------------------------
-                SCLK            => SCLK_tb,
-                MOSI            => MOSI_tb,
-                SS              => SS_tb,
-                MISO            => MISO_tb,
-                ------------------------------------
-                SEND_DATA_I     => data_send_of_master,
-                READY_O         => ready_sender_slave,
-                VALID_I         => valid_sender_slave,
-                ------------------------------------
-                RECEIVE_DATA_O  => DATA_RECEIVED_of_master,
-                READY_I         => ready_err,
-                VALID_O         => valid_err
-            );
+    ins0_spi_slave : entity work.spi_slave 
+        generic map(
+            data_TX_spi_slave_reg_width => 8,
+            data_RX_spi_slave_reg_width => 8,
+            CPOL                        => '1',
+            CPHA                        => '0'
+            -- MSB_first       : integer := 1 
+            -- LSB_first       : integer := 0
+        )
+        port map(
+            CLK_I           => CLK_TOP,
+            RESET_I         => RESET_TOP,
+            ------------------------------------
+            SCLK            => SCLK_tb,
+            MOSI            => MOSI_tb,
+            SS              => SS_tb,
+            MISO            => MISO_tb,
+            ------------------------------------
+            SEND_DATA_I     => data_send_of_master,
+            READY_O         => ready_sender_slave,
+            VALID_I         => valid_sender_slave,
+            ------------------------------------
+            RECEIVE_DATA_O  => data_err,
+            READY_I         => ready_err,
+            VALID_O         => valid_err
+        );
+
+    ins0_err : entity  work.ERROR_CHECK
+        generic map(
+            number_of_bit_DATA_I   => 8,
+            number_of_bit_ERROR_O  => 4
+        )
+        port map(
+            CLK_I     => CLK_TOP,
+            RESET_I   => RESET_TOP,
+            VALID_I   => valid_err,
+            DATA_I    => data_err,
+            ERROR_O   => ERROR_TOP,
+            READY_O   => ready_err
+        );
 
 end architecture structrual;  
  
--- src/hdl/top_tb.vhd
+++ src/hdl/top_tb.vhd
@@ -6,11 +6,11 @@ end;
 
 architecture testbench of top_tb is
        
-signal enable_tb                    : std_logic; 
-signal reset_tb                     : std_logic; 
-signal clk_tb                       : std_logic;
-signal start_send_tb                : std_logic;
-signal DATA_RECEIVED_of_master_tb   : std_logic_vector(7 downto 0);
+signal enable_tb        : std_logic; 
+signal reset_tb         : std_logic; 
+signal clk_tb           : std_logic;
+signal start_send_tb    : std_logic;
+signal ERROR_TOP_tb     : std_logic_vector(3 downto 0);
 
 constant T_10      : time    := 10    ns;
 
@@ -18,23 +18,23 @@ begin
 
     ins_0 : entity work.top
         port map(
-            RESET_TOP                => reset_tb,
-            CLK_TOP                  => clk_tb,
-            ENABLE_TOP               => enable_tb,
-            START_SEN_TOP            => start_send_tb,
-            DATA_RECEIVED_of_master  => DATA_RECEIVED_of_master_tb
+            RESET_TOP       => reset_tb,
+            CLK_TOP         => clk_tb,
+            ENABLE_TOP      => enable_tb,
+            START_SEN_TOP   => start_send_tb,
+            ERROR_TOP       => ERROR_TOP_tb
         );
 
-process
-begin
-    clk_tb  <= '0' ;
-    wait for T_10/2;
-    clk_tb  <= '1' ;
-    wait for T_10/2;
-end process;
+    process
+    begin
+        clk_tb  <= '0' ;
+        wait for T_10/2;
+        clk_tb  <= '1' ;
+        wait for T_10/2;
+    end process;
 
-reset_tb        <= '1'  , '0' after (T_10)/2, '1' after 20 us, '0' after 30 us ;
-enable_tb       <= '0'  , '1' after 50 ns;
-start_send_tb   <= '1' after 20 ns, '0' after 50 us;
+    reset_tb        <= '1'  , '0' after (T_10)/2, '1' after 20 us, '0' after 30 us ;
+    enable_tb       <= '0'  , '1' after 50 ns;
+    start_send_tb   <= '1' after 20 ns, '0' after 50 us;
 
 end architecture testbench;
